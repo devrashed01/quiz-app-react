6e5f8dfd1955bc43c42f1b1dce61562505a871c8
devrashed01 <devrashed01@gmail.com>
2024-02-24 17:10:46 +0600

    Update QuizCompleteScreen styling

--- src/features/answers/QuizCompleteScreen.tsx
+++ src/features/answers/QuizCompleteScreen.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 export default function QuizCompleteScreen({ onClose }: Props) {
   return (
-    <Card className="p-10 flex flex-col items-center justify-center gap-10">
+    <Card className="p-10 flex flex-col items-center text-center justify-center gap-10">
       <h3 className="text-2xl text-slate-700">You have successfully completed the quiz</h3>
       <Button onClick={onClose}>Okay</Button>
     </Card>
